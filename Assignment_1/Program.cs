f28aae56ca945efd89d252d5506bacc0c19c0538
Noushin <n01464164@humbermail.ca>
2020-10-21 17:30:10 -0400

    Assignment

---
+++ Assignment_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Assignment_1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers following '+' in order to calculate the total (Ex.2+3+5): ");
+
+            string input = Console.ReadLine();
+            string[] splitInput = input.Split('+');
+
+            long sum = 0;
+            foreach (string number_str in splitInput)
+            {
+                try
+                {
+                    long number = Int64.Parse(number_str);
+                    sum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} is not an integer.", number_str);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The number {0} is too large", number_str);
+                }
+            }
+            Console.WriteLine("output: {0} ", sum);
+        }
+    }
+}
